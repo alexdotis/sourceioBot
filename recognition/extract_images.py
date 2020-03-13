7daa09ea06b16d01b99421213ae61d1b438b8a5c
alexdotis <43515852+alexdotis@users.noreply.github.com>
2020-03-13 03:02:36 +0200

    Add files via upload

---
+++ recognition/extract_images.py
@@ -0,0 +1,141 @@
+import json
+import requests
+import os
+import pytesseract
+from io import BytesIO
+from PIL import Image
+import sys
+
+IMAGES_URL = "http://s0urce.io/client/img/word/{key}/{value}"
+MAIN_URL = "http://s0urce.io"
+FILE_IMAGES = "hackergame_images"
+order = {"e": 62, "m": 66, "h": 55}
+
+# If you don't have tesseract executable in your PATH, include the following:
+# TESSERACT_EXECUTABLE_PATH = r'<full_path_to_your_tesseract_executable>
+TESSERACT_EXECUTABLE_PATH = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# In my case is r'C:\Program Files\Tesseract-OCR\tesseract.exe
+
+__all__ = ("start",)
+
+ALL_WORDS = ['get', 'add', 'dir', 'list', 'file', 'ghost', 'http', 'pass', 'ping', 'port', 'write', 'remove', 'anon',
+             'user', 'bytes', 'socket', 'net', 'val', 'count', 'point', 'event', 'reset', 'com', 'signal', 'load',
+             'handle', 'key', 'stat', 'buffer', 'send', 'host', 'delete', 'bit', 'size', 'upload', 'poly', 'temp',
+             'system', 'root', 'client', 'data', 'init', 'info', 'xml', 'log', 'loop', 'global', 'cookies', 'part',
+             'emit', 'set', 'url', 'call', 'type', 'add', 'left', 'join', 'status', 'right', 'num', 'domain', 'intel',
+             'constructor', 'encryptfile', 'getpass', 'responder', 'setstats', 'encrypt', 'newline', 'mysql',
+             'writefile', 'eventtype', 'accountname', 'download', 'setcookie', 'setnewid', 'command', 'thread',
+             'process', 'listconfig', 'syscall', 'proxy', 'filedir', 'newhost', 'getid', 'server', 'number', 'sizeof',
+             'module', 'urlcheck', 'gridwidth', 'decrypt', 'datatype', 'config', 'getinfo', 'userport', 'hostserver',
+             'account', 'generate', 'filetype', 'decryptfile', 'setport', 'threat', 'package', 'userid', 'loadbytes',
+             'channel', 'hexagon', 'disconnect', 'getfile', 'encode', 'protocol', 'password', 'getkey', 'serverproxy',
+             'gridheight', 'getping', 'getlog', 'export', 'connect', 'newserver', 'findpackage', 'length', 'vector',
+             'fillgrid', 'username', 'response', 'setping', 'joinnetworkclient', 'unpacktmpfile', 'getdatapassword',
+             'bufferpingset', 'emitconfiglist', 'rootcookieset', 'setnewproxy', 'sizeofhexagon', 'getmysqldomain',
+             'callmodule', 'getfirewallchannel', 'changeusername', 'httpbuffersize', 'removeoldcookie',
+             'fileexpresslog', 'getpartoffile', 'disconnectserver', 'batchallfiles', 'exportconfigpackage',
+             'statusofprocess', 'getxmlprotocol', 'loadloggedpassword', 'sendintelpass', 'eventlistdir',
+             'changepassword', 'uploaduserstats', 'decryptdatabatch', 'loadregisterlist', 'systemgridtype',
+             'encodenewfolder', 'encryptunpackedbatch', 'destroybatch', 'dodecahedron', 'respondertimeout',
+             'channelsetpackage', 'checkhttptype', 'tempdatapass', 'blockthreat', 'deleteallids', 'removenewcookie',
+             'ghostfilesystem', 'includedirectory', 'create3axisvector', 'systemportkey', 'mergesocket',
+             'patcheventlog', 'createnewpackage', 'disconnectchannel', 'createfilethread', 'wordcounter',
+             'create2axisvector', 'generatecodepack', 'createnewsocket', 'hostnewserver', 'loadaltevent']
+
+
+def create_files():
+    """Creating files of Images"""
+    if not os.path.exists(FILE_IMAGES):
+        os.mkdir(FILE_IMAGES)
+        for keys in order.keys():
+            try:
+                os.mkdir(os.path.join(FILE_IMAGES, keys))
+            except FileExistsError:
+                print("FileExistsError")
+                sys.exit(1)
+        return True
+    else:
+        return False
+
+
+def download_images():
+    """Downloading images"""
+    if create_files():
+        print("[*] Download Images, Please Wait...")
+        for keys, values in order.items():
+            for value in range(values):
+                data = requests.get(IMAGES_URL.format(key=keys, value=value))
+                if data.status_code == 200:
+                    images = Image.open(BytesIO(data.content))
+                    images.save(os.path.join(os.path.join(FILE_IMAGES, keys), "{}{}.png".format(keys, value)))
+        return search_and_edit_images()
+
+
+def ocr_image(IM):
+    """Recognize Text"""
+    if TESSERACT_EXECUTABLE_PATH is not None:
+        pytesseract.pytesseract.tesseract_cmd = TESSERACT_EXECUTABLE_PATH
+    word = pytesseract.image_to_string(Image.open(IM))
+    return word.lower()
+
+
+def _edit_image(image, size=None):
+    """Editing images for text recognition"""
+    if size is not None:
+        pensize = Image.open(image)
+        im = pensize.convert("RGB").resize((300, size))
+    else:
+        im = Image.open(image).convert("RGB")
+
+    new = Image.new("RGB", (im.width, im.height))
+    for i in range(im.width):
+        for j in range(im.height):
+            r, g, b = im.getpixel((i, j))
+            if r == g:
+                new.putpixel((i, j), (0, 0, 0))
+            else:
+                new.putpixel((i, j), (255, 255, 255))
+    new.save("digital.png")
+    return "digital.png"
+
+
+def search_and_edit_images():
+    print("[+] editing images, please wait...".title())
+    word_dictionary = {}
+
+    for root, dirs, files in os.walk(FILE_IMAGES):
+        for filenames in files:
+            if filenames.endswith(".png"):
+
+                picture_path = os.path.join(root, filenames)
+                processing_image = _edit_image(picture_path)
+                image_to_word = ocr_image(processing_image)
+
+                if image_to_word not in ALL_WORDS:
+                    size = 20
+
+                    while image_to_word not in ALL_WORDS:
+                        processing_image = _edit_image(picture_path, size=size)
+                        image_to_word = ocr_image(processing_image)
+                        size += 5
+                        if size > 200:
+                            word_dictionary[filenames] = None
+                            break
+                    else:
+                        word_dictionary[filenames] = image_to_word
+                else:
+                    word_dictionary[filenames] = image_to_word
+
+    with open("wordlist.txt", "w") as f:
+        json.dump(word_dictionary, f, indent=4)
+    try:
+        os.remove("digital.png")
+        print("[!] Done!")
+        sys.exit(1)
+    except Exception as e:
+        print(str(e))
+        pass
+
+
+def start():
+    download_images()
---
+++ sourceioBot.py
@@ -0,0 +1,147 @@
+from selenium import webdriver
+import random
+import time
+import json
+import re
+import decimal
+from recognition import extract_images
+
+extract_images.start()
+
+#CHOMEDRIVER_PATH = "r'<full_path_to_your_chromedriver.exe>
+CHOMEDRIVER_PATH = r"D:\downloads\chromedriver.exe"
+
+return_values = {"ammount": '$("#window-my-coinamount").text()',
+                 "bot_net": '$("#shop-bot-net-value").text()',
+                 "quantum": '$("#shop-quantum-server-value").text()',
+                 "max_charges": '$("#shop-max-charges").text()',
+                 "max_strength": '$("#shop-strength").text()',
+                 "max_regen": '$("#shop-regen").text()',
+                 "price_max_charges": '$("#shop-firewall-max_charge10-value").text()',
+                 "price_shop_strength": '$("#shop-firewall-difficulty-value").text()',
+                 "price_shop_regen": '$("#shop-firewall-regen-value").text()'}
+
+clicks = {"quantum": '$("#shop-quantum-server-value").click()',
+          "botnet": '$("#shop-bot-net-value").click()',
+          "max_charges": '$("#shop-firewall-max_charge10").click()',
+          "shop_strength": '$("#shop-firewall-difficulty").click()',
+          "shop_regen": '$("#shop-firewall-regen-value").click()'}
+
+firewall_config_charges = {'max_charges': 30, 'max_strength': 4, 'max_regen': 10}
+
+
+class SourceIoBot:
+
+    def __init__(self, username):
+        self.username = username
+        self.url = "http://s0urce.io/"
+        if CHOMEDRIVER_PATH is not None:
+            self.driver = webdriver.Chrome(CHOMEDRIVER_PATH)
+        else:
+            self.driver = webdriver.Chrome()
+        self.driver.get(self.url)
+        time.sleep(3)
+
+    def __str__(self):
+        return self.username
+
+    def login(self):
+        self.driver.find_element_by_id("login-input").send_keys(self.username)
+        submit = self.driver.find_element_by_tag_name("form")
+        submit.submit()
+        time.sleep(2)
+
+    def find_target(self):
+        player_list = f'$("#player-list").children("tr")[{int(random.randrange(0, 5))}].click()'
+        self.driver.execute_script(player_list)
+        self.driver.execute_script('$("#window-other-button").click()')
+        self.driver.execute_script('$("#window-other-port{}").click()'.format(random.randrange(1, 3)))
+        time.sleep(0.5)
+
+    def attack(self):
+        time.sleep(3)
+        while True:
+            self.find_target()
+            progress_bar = self.driver.execute_script('return $("#progressbar-firewall-amount").attr("style")')
+            percentage_of_progress_bar = "".join(re.findall(r'[0-9]+', progress_bar))
+
+            while int(percentage_of_progress_bar) <= 100:
+                icon = self.driver.find_elements_by_class_name("tool-type-img")
+                for image in icon:
+                    link = image.get_attribute('src')
+                if link == "http://s0urce.io/client/img/words/template.png":
+                    break
+                image_link = "".join(link.split("/")[6:]) + ".png"
+                type_word = find_word(image_link)
+                self.driver.execute_script('$("#tool-type-word").val("{}")'.format(type_word))
+                self.driver.execute_script('$("#tool-type-word").val("{}").submit()'.format(type_word))
+                progress_bar = self.driver.execute_script('return $("#progressbar-firewall-amount").attr("style")')
+                percentage_of_progress_bar = "".join(re.findall(r'[0-9]+', progress_bar))
+                time.sleep(.7)
+                self.driver.execute_script('$("#targetmessage-button-send").click()')
+            self.firewalls()
+            self.buy_mines()
+
+    def firewalls(self):
+        for firewall in range(1, 4):
+            to_firewall = f'$("#window-firewall-part{str(firewall)}").click()'
+            self.driver.execute_script(to_firewall)
+            time.sleep(.4)
+            max_charges = self.driver.execute_script('return {}'.format(return_values["max_charges"]))
+            max_strength = self.driver.execute_script('return {}'.format(return_values["max_strength"]))
+            max_regen = self.driver.execute_script('return {}'.format(return_values["max_regen"]))
+            price_max_charges = self.driver.execute_script("return {}".format(return_values["price_max_charges"]))
+            price_shop_strength = self.driver.execute_script("return {}".format(return_values["price_shop_strength"]))
+            price_shop_regen = self.driver.execute_script("return {}".format(return_values["price_shop_regen"]))
+            bit_coins = self.driver.execute_script("return {}".format(return_values["ammount"]))
+
+            if int(max_charges) < firewall_config_charges["max_charges"] and \
+                    to_decimal(bit_coins) > to_decimal(price_max_charges):
+                self.driver.execute_script("{}".format(clicks["max_charges"]))
+
+            if int(max_strength) < firewall_config_charges["max_strength"] \
+                    and to_decimal(bit_coins) > to_decimal(price_shop_strength):
+                self.driver.execute_script("{}".format(clicks["shop_strength"]))
+
+            if int(max_regen) < firewall_config_charges["max_regen"] \
+                    and to_decimal(bit_coins) > to_decimal(price_shop_regen):
+
+                self.driver.execute_script("{}".format(clicks["shop_regen"]))
+
+            else:
+                self.driver.execute_script('$("#window-firewall-pagebutton").click()')
+            self.driver.execute_script('$("#window-firewall-pagebutton").click()')  # back button
+
+    def buy_mines(self):
+        mine_botnet_price = self.driver.execute_script('return {}'.format(return_values["bot_net"]))
+        quantum_price = self.driver.execute_script('return {}'.format(return_values["quantum"]))
+        bit_coins = self.driver.execute_script("return {}".format(return_values["ammount"]))
+
+        while to_decimal(bit_coins) > to_decimal(quantum_price):
+            self.driver.execute_script('{}'.format(clicks["quantum"]))
+            quantum_price = self.driver.execute_script('return {}'.format(return_values["quantum"]))
+            bit_coins = self.driver.execute_script("return {}".format(return_values["ammount"]))
+            time.sleep(.5)
+
+        while to_decimal(bit_coins) > to_decimal(mine_botnet_price):
+            self.driver.execute_script('{}'.format(clicks["botnet"]))
+            mine_botnet_price = self.driver.execute_script('return {}'.format(return_values["bot_net"]))
+            bit_coins = self.driver.execute_script("return {}".format(return_values["ammount"]))
+            time.sleep(.5)
+
+
+def to_decimal(value):
+    return decimal.Decimal(value)
+
+
+def find_word(link):
+    with open("wordlist.txt", "r") as f:
+        word = json.loads(f.read())
+    return word[link]
+
+
+if __name__ == '__main__':
+    USERNAME = "YOURYOUSERNAME"
+    S = SourceIoBot(USERNAME)
+    S.login()
+    S.attack()
