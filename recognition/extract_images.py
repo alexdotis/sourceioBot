3afae87cedb771c62d8d5d3a9371010e4bc7b6f3
alexdotis <43515852+alexdotis@users.noreply.github.com>
2021-09-18 19:50:58 +0300

    Update extract_images.py

--- recognition/extract_images.py
+++ recognition/extract_images.py
@@ -1,22 +1,19 @@
-import json
+from typing import Tuple, Optional
+import PIL
 import requests
-import os
-import pytesseract
-from io import BytesIO
 from PIL import Image
-import sys
-
-IMAGES_URL = "http://s0urce.io/client/img/word/{key}/{value}"
-MAIN_URL = "http://s0urce.io"
-FILE_IMAGES = "hackergame_images"
-order = {"e": 62, "m": 66, "h": 55}
+import pytesseract
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from io import BytesIO
+from difflib import SequenceMatcher
+import json
+import time
 
 # If you don't have tesseract executable in your PATH, include the following:
 # TESSERACT_EXECUTABLE_PATH = r'<full_path_to_your_tesseract_executable>
 TESSERACT_EXECUTABLE_PATH = None
 # In my case is r'C:\Program Files\Tesseract-OCR\tesseract.exe
-
-__all__ = ("start",)
 
 ALL_WORDS = ['get', 'add', 'dir', 'list', 'file', 'ghost', 'http', 'pass', 'ping', 'port', 'write', 'remove', 'anon',
              'user', 'bytes', 'socket', 'net', 'val', 'count', 'point', 'event', 'reset', 'com', 'signal', 'load',
@@ -43,99 +40,112 @@ ALL_WORDS = ['get', 'add', 'dir', 'list', 'file', 'ghost', 'http', 'pass', 'ping
              'create2axisvector', 'generatecodepack', 'createnewsocket', 'hostnewserver', 'loadaltevent']
 
 
-def create_files():
-    """Creating files of Images"""
-    if not os.path.exists(FILE_IMAGES):
-        os.mkdir(FILE_IMAGES)
-        for keys in order.keys():
-            try:
-                os.mkdir(os.path.join(FILE_IMAGES, keys))
-            except FileExistsError:
-                print("FileExistsError")
-                sys.exit(1)
-        return True
-    else:
-        return False
+class Recognition:
+    def __init__(self, image: PIL.Image, file_name: str = '') -> None:
+        self.image = image
+        self.file_name = file_name
 
+    @classmethod
+    def read_from_url(cls, url: str) -> 'Recognition':
+        """Reading the image link creates a folder with the image name and saves it"""
 
-def download_images():
-    """Downloading images"""
-    if create_files():
-        print("[*] Download Images, Please Wait...")
-        for keys, values in order.items():
-            for value in range(values):
-                data = requests.get(IMAGES_URL.format(key=keys, value=value))
-                if data.status_code == 200:
-                    images = Image.open(BytesIO(data.content))
-                    images.save(os.path.join(os.path.join(FILE_IMAGES, keys), "{}{}.png".format(keys, value)))
-        return search_and_edit_images()
+        """From the following link for example "http://s0urce.io/client/img/word/m/1" we get the last two items """
+        image_name = url.split('/')[-2:]  # ['m','1']
 
+        folder_path = Path(f'images/{image_name[0]}')
+        folder_path.mkdir(parents=True, exist_ok=True)
 
-def ocr_image(IM):
-    """Recognize Text"""
-    if TESSERACT_EXECUTABLE_PATH is not None:
-        pytesseract.pytesseract.tesseract_cmd = TESSERACT_EXECUTABLE_PATH
-    word = pytesseract.image_to_string(Image.open(IM))
-    return word.lower()
+        with requests.get(url) as response:
+            response.raise_for_status()
+            img = Image.open(BytesIO(response.content))
+            filename = f'{"".join(image_name)}.png'
+            img.save(folder_path / filename)
+        return cls(img, filename)
 
+    def edit_image(self, size: Optional[Tuple] = None) -> PIL.Image:
+        """Clean the image and turn all the colors to white except the letters
+        Args:
+            size (Optional[Tuple], optional): [description]. Defaults to None.
 
-def _edit_image(image, size=None):
-    """Editing images for text recognition"""
-    if size is not None:
-        pensize = Image.open(image)
-        im = pensize.convert("RGB").resize((300, size))
-    else:
-        im = Image.open(image).convert("RGB")
-
-    new = Image.new("RGB", (im.width, im.height))
-    for i in range(im.width):
-        for j in range(im.height):
-            r, g, b = im.getpixel((i, j))
-            if r == g:
-                new.putpixel((i, j), (0, 0, 0))
-            else:
-                new.putpixel((i, j), (255, 255, 255))
-    new.save("digital.png")
-    return "digital.png"
-
-
-def search_and_edit_images():
-    print("[+] editing images, please wait...".title())
-    word_dictionary = {}
-
-    for root, dirs, files in os.walk(FILE_IMAGES):
-        for filenames in files:
-            if filenames.endswith(".png"):
-
-                picture_path = os.path.join(root, filenames)
-                processing_image = _edit_image(picture_path)
-                image_to_word = ocr_image(processing_image)
-
-                if image_to_word not in ALL_WORDS:
-                    size = 20
-
-                    while image_to_word not in ALL_WORDS:
-                        processing_image = _edit_image(picture_path, size=size)
-                        image_to_word = ocr_image(processing_image)
-                        size += 5
-                        if size > 200:
-                            word_dictionary[filenames] = None
-                            break
-                    else:
-                        word_dictionary[filenames] = image_to_word
+        Returns:
+            PIL.Image: Edited image
+        """
+        image = self.image.convert(('RGB'))
+        edited_image = Image.new('RGB', (image.width, image.height))
+        for i in range(image.width):
+            for j in range(image.height):
+                r, g, _ = image.getpixel((i, j))
+                if r == g:
+                    edited_image.putpixel((i, j), (0, 0, 0))
                 else:
-                    word_dictionary[filenames] = image_to_word
+                    edited_image.putpixel((i, j), (255, 255, 255))
+        if size is not None:
+            return edited_image.resize(size)
+        return edited_image
 
-    with open("wordlist.txt", "w") as f:
-        json.dump(word_dictionary, f, indent=4)
-    try:
-        os.remove("digital.png")
-        print("[!] Done!")
-        sys.exit(1)
-    except Exception as e:
-        print(str(e))
-        pass
+    def get_image_text(self) -> str:
+        """With the help of pytesseract we get the word of image and match it with ALL_WORDS
+         If necessary, resize the image
+
+        Returns:
+            str: Word of Image
+        """
+        img = self.edit_image()
+        fetch_word = pytesseract.image_to_string(img).strip()
+        ratio, word = max(
+            [(SequenceMatcher(None, fetch_word, w).ratio(), w) for w in ALL_WORDS])
+        while ratio <= 0.75:
+            x, _ = size = (200, 100)
+            img = self.edit_image(size)
+            if TESSERACT_EXECUTABLE_PATH is not None:
+                pytesseract.pytesseract.tesseract_cmd = TESSERACT_EXECUTABLE_PATH
+            fetch_word = pytesseract.image_to_string(img).strip()
+            ratio, word = max(
+                [(SequenceMatcher(None, fetch_word, w).ratio(), w) for w in ALL_WORDS])
+            x += 50
+            if x > 1000:
+                break
+
+        return word.lower()
 
 
-def start():
-    download_images()
+def create_wordlist() -> None:
+    """The final result is the wordlist
+    """
+
+    ORDER = {"e": 62, "m": 66, "h": 55}
+    BASE_URL = "http://s0urce.io/client/img/word/{key}/{value}"
+
+    urls = [
+        BASE_URL.format(key=key, value=value) for key, values in ORDER.items() for value in range(values)
+    ]
+    words = {}
+    print('[!] Please wait...')
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = [executor.submit(Recognition.read_from_url, url)
+                   for url in urls]
+        for res in as_completed(results):
+            filename = res.result().file_name
+            word = Recognition(res.result().image).get_image_text()
+            words[filename] = word
+
+    with open('wordlist.json', 'w') as f:
+        json.dump(words, f, indent=4)
+
+    print('[*] Images and WordList are ready...')
+
+
+def nulls_in_wordlist() -> bool:
+    with open('wordlist.json', 'r') as f:
+        content = json.load(f)
+
+    nulls = [key for key, value in content.items() if value is None]
+
+    if nulls:
+        print('[!] Please fill the empty fields on wordlist.json')
+        print('\n'.join(nulls))
+        return True
+    return False
+
+
+
